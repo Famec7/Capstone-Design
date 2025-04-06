edfc102a4280a82ffa96be8bb7261e2ce732713c
choiwoohyck <xxxxd1000@naver.com>
2025-04-06 14:05:46 +0900

    feat: 나무 채집물 타격포인트 생성 & 타격 각도 재조정
    feat: 나무 채집물 타격포인트 생성 & 타격 각도 재조정

--- Assets/Scripts/Object/HarvestableObject/HarvestableObject.cs
+++ Assets/Scripts/Object/HarvestableObject/HarvestableObject.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HarvestableObject : MonoBehaviour,IDamageable,ILootDrop
@@ -10,6 +11,9 @@ public class HarvestableObject : MonoBehaviour,IDamageable,ILootDrop
     // 런타임 내구도 관리
     protected int currentDurability;
 
+    public Vector3 Center;
+    public float Radius = 1f;
+    private float _randomAngle;
     protected virtual void Awake()
     {
         // ScriptableObject에 설정된 내구도로 초기화
@@ -21,6 +25,16 @@ public class HarvestableObject : MonoBehaviour,IDamageable,ILootDrop
         {
             Debug.LogError($"{gameObject.name}의 harvestData가 할당되지 않았습니다!");
         }
+
+        Center = transform.position;
+        _randomAngle = Random.Range(0f, 2 * Mathf.PI);
+        transform.position = new Vector3(Center.x + Radius * Mathf.Cos(_randomAngle), Center.y, Center.z + Radius * Mathf.Sin(_randomAngle));
+    }
+
+    public void Chop(int attackPower)
+    {
+        int damage = attackPower; // 필요하면 angularSpeed를 곱하는 등 가중치를 추가
+        TakeDamage(damage);
     }
 
     // 피해를 받을 때 호출되는 메서드
@@ -54,4 +68,11 @@ public class HarvestableObject : MonoBehaviour,IDamageable,ILootDrop
 #endif
         }
     }
+
+    // 원주 안에 랜덤 좌표 배치
+    public void SetRandomPos()
+    {
+        _randomAngle += Random.Range(10f, 30f) * Mathf.Deg2Rad;
+        transform.position = new Vector3(Center.x + Radius * Mathf.Cos(_randomAngle), Center.y + Random.Range(-0.1f,0.1f), Center.z + Radius * Mathf.Sin(_randomAngle));
+    }   
 }
