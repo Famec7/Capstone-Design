d13ac69b4ab5a8f178dae3e8944e5e734460d4a1
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-04-24 23:20:38 +0900

    Revert "Merge branch 'develop' into feat/escapeUI"
    This reverts commit fad029c98c143e10b66c55cdd4c89422f0802e19, reversing
    changes made to 3300953f9061f158d65bf27e21cdacf573082549.

--- Assets/Scripts/Item/BaseItem.cs
+++ Assets/Scripts/Item/BaseItem.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public abstract class BaseItem : MonoBehaviour,ISellable
 {
     public ItemData Data;
-    public bool IsInInventory = false;
     public void SellToShop()
     {
         ;
@@ -20,48 +19,5 @@ public abstract class BaseItem : MonoBehaviour,ISellable
     public virtual void OnDeselected()
     {
         Debug.Log($"{Data.ItemName} 선택 해제됨");
-    }
-
-    public void Update()
-    {
-        if(IsInInventory)
-        {
-            Rigidbody rb = GetComponent<Rigidbody>();
-
-            rb.isKinematic = true;
-            rb.useGravity = false;
-            rb.constraints = RigidbodyConstraints.FreezeAll;
-
-            transform.localScale = Vector3.one;
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
-        }
-    }
-
-    public void InitTransform()
-    {
-        Rigidbody rb = GetComponent<Rigidbody>();
-
-        rb.isKinematic = false;
-        rb.useGravity = true;
-        rb.constraints = RigidbodyConstraints.None;
-
-        // 월드 스케일을 1,1,1로 만들기 위해 부모 스케일에 반비례하게 조정
-        if (transform.parent != null)
-        {
-            Vector3 parentScale = transform.parent.lossyScale;
-            transform.localScale = new Vector3(
-                1f / parentScale.x,
-                1f / parentScale.y,
-                1f / parentScale.z
-            );
-        }
-        else
-        {
-            transform.localScale = Vector3.one;
-        }
-
-        transform.localPosition = Vector3.zero;
-        transform.localRotation = Quaternion.identity;
     }
 }
---
+++ Assets/Scripts/UI/Escape/EscapeUIManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeUIManager : MonoBehaviour
+{
+    public static EscapeUIManager Instance;
+
+    [Header("UI Root")]
+    [SerializeField] private GameObject uiRoot;   
+
+    [Header("Requirements")]
+    [SerializeField] private int requiredWood = 10;
+    [SerializeField] private int requiredStone = 10;
+
+    [Header("Slots")]
+    [SerializeField] private List<RepairSlotHighlighter> woodSlots;
+    [SerializeField] private List<RepairSlotHighlighter> stoneSlots;
+
+    int currentWood, currentStone;
+
+    void Awake()
+    {
+        Instance = this;
+        uiRoot.SetActive(false);
+    }
+
+    public void ShowUI(bool show) => uiRoot.SetActive(show);
+
+    public void RegisterMaterial(MaterialType type, Sprite icon)
+    {
+        if (type == MaterialType.Wood && currentWood < requiredWood)
+        {
+            woodSlots[currentWood].itemIcon.sprite = icon;
+            woodSlots[currentWood].itemIcon.color = Color.white;
+            currentWood++;
+        }
+        else if (type == MaterialType.Stone && currentStone < requiredStone)
+        {
+            stoneSlots[currentStone].itemIcon.sprite = icon;
+            stoneSlots[currentStone].itemIcon.color = Color.white;
+            currentStone++;
+        }
+        CheckComplete();
+    }
+
+    void CheckComplete()
+    {
+        if (currentWood >= requiredWood && currentStone >= requiredStone)
+        {
+            Debug.Log("모든 재료가 모였습니다! 탈출(수리) 처리 실행");
+        }
+    }
+}
